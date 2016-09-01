a3d6d4d395477b5c713b0eae95ccd7bd54fd244f
Danb8 <danb1459@gmail.com>
2016-09-01 12:13:47 -0400

    Add files via upload

---
+++ Numbers/Find_pi_to_the_nth_digit.java
@@ -0,0 +1,31 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class Find_pi_to_the_nth_digit {
+
+	public static void main(String[] args) {
+		Double pi = Math.PI;
+
+		String piString = pi.toString();
+		int N;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Finding pi to the Nth decimal place. Please enter an integer N between 1 and 15.");
+		N = input.nextInt();
+
+		String result = "";
+		
+		if (N < 1 || N > 15) {
+			System.out.println("Not valid. Please enter an integer between 1 and 15");
+			N = input.nextInt();
+		}
+
+		for (int i = 0; i <= N + 1; i++) {
+			result += piString.charAt(i);
+
+		}
+		
+		System.out.println(result);
+	}
+
+}
